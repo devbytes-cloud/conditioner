cb6403c88e654581ecdba5c2096c68b326863ab4
David Dymko <dymkod@gmail.com>
2024-06-29 21:20:16 -0400

    feat: add config creation with allowlist and whoami support

--- cmd/kubectl-conditioner.go
+++ cmd/kubectl-conditioner.go
@@ -1,13 +1,30 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/devbytes-cloud/conditioner/pkg/cmd"
 	"github.com/spf13/pflag"
-
 	"k8s.io/cli-runtime/pkg/genericiooptions"
+
+	"github.com/devbytes-cloud/conditioner/pkg/config"
 )
+
+func init() {
+	exists, err := config.Exists()
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	if !exists {
+		if err := config.Write(); err != nil {
+			fmt.Println(err.Error())
+			os.Exit(1)
+		}
+	}
+}
 
 func main() {
 	flags := pflag.NewFlagSet("kubectl-conditioner", pflag.ExitOnError)
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/devbytes-cloud/conditioner
 go 1.22.0
 
 require (
+	github.com/mitchellh/go-homedir v1.1.0
 	github.com/spf13/cobra v1.8.1
 	github.com/spf13/pflag v1.0.5
 	github.com/stretchr/testify v1.9.0
